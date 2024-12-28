badcae247829b7bb73a1da185a0321ef5787c3ce
theredwiking <hjulmandn@gmail.com>
2024-12-28 14:46:22 +0100

    Updated nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -73,7 +73,7 @@ return {
       end)
 
       require('mason-lspconfig').setup({
-        ensure_installed = {},
+        ensure_installed = {'gopls', 'html', 'ts_ls'},
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
